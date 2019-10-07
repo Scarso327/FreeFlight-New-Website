8ae623bf75a4777bdd546e29b2f86e40bebefb88
Jack Farhall <jackfarhall@gmail.com>
2019-10-07 18:01:32 +0100

    Cleanup

--- views/forums/topic.php
+++ views/forums/topic.php
@@ -11,24 +11,22 @@
             </div>
         </div>
     </section>
-
     <section class="forum-section">
-            <div class="forum-header topic-header reply" id="<?=$this->topic->id;?>">
-                <div class="topic-header-info">
-                    <p>Topic by <a href="<?=URL;?>forums/user/<?=$this->topic->steamID;?>"><?=$this->topic->steamName;?></a>, <?=Application::formatTime($this->topic->posted);?></p>
-                </div>
+        <div class="forum-header topic-header reply" id="<?=$this->topic->id;?>">
+            <div class="topic-header-info">
+                <p>Topic by <a href="<?=URL;?>forums/user/<?=$this->topic->steamID;?>"><?=$this->topic->steamName;?></a>, <?=Application::formatTime($this->topic->posted);?></p>
             </div>
-            <div class="topic-content reply">
-                <div class="reply-author">
-                    <img src="<?=$this->topic->steampfplarge;?>"/>
-                    <h4 class="title">Member</h4>
-                </div>
-                <div class="reply-body">
-                    <?=$this->topic->content;?>
-                </div>
+        </div>
+        <div class="topic-content reply">
+            <div class="reply-author">
+                <img src="<?=$this->topic->steampfplarge;?>"/>
+                <h4 class="title">Member</h4>
             </div>
-        </section>
-
+            <div class="reply-body">
+                <?=$this->topic->content;?>
+            </div>
+        </div>
+    </section>
     <?php
     foreach($this->topic->replies as $reply) {
         ?>
